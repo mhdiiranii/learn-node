d5426aa7f93ffa12b5b3d087919e2b78af1a29fd
MAHDI <mhdiiranii@gmail.com>
2024-07-09 18:20:44 +0330

    middleware

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const userRoute = require ('./routes/userRoute')
 const morgan = require ('morgan')
+const check = require ('./middleware ')
 
 
 const app = express();
 
-app.use(morgan())
+app.use(morgan('dev'))
+app.use((req, res, next)=>{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  // res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
 app.use('/api',userRoute)
 
 // app.get("/api", getAllUser);
